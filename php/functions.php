4a54ff288a378d1d7fd594246bc6806b3e9f700f
Yami <gbwalleyeseries@gmail.com>
2020-01-28 22:29:43 -0600

    a

--- php/functions.php
+++ php/functions.php
@@ -73,8 +73,10 @@ function get_team($last_name) {
     if ($result->num_rows > 0) {
         while($row = mysqli_fetch_array($result)) {
             $mbr_id=$row['mbr_id'];
-            $team_sql="SELECT * from team_info where partner1='".$mbr_id."' or partner2='".$mbr_id."' or sub1='".$mbr_id."' or sub2='".$mbr_id."'";
-            $team_result += $mysqli->query($team_sql);
+            echo $mbr_id;
+            //$team_sql="SELECT * from team_info where partner1='".$mbr_id."' or partner2='".$mbr_id."' or sub1='".$mbr_id."' or sub2='".$mbr_id."'";
+            //$team_result = $mysqli->query($team_sql);
+            //echo $team_result;
         }
     }
     return $team_result;
