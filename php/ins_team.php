0b25fa559bd5d1db5db6466a6cc2d4b4dc42c93f
Yami <gbwalleyeseries@gmail.com>
2020-02-17 20:07:18 -0600

    a

--- php/ins_team.php
+++ php/ins_team.php
@@ -138,8 +138,7 @@ $mysqli->close();
 echo "<br>";
 
 echo "<a href=../sel_tourney.php?team_id=".$team_id."&team_reg=".$reg_size.">Click here if not automatically redirected to tournament registration page</a>";
-header("location:../sel_tourney.php?team_id=$team_id&team_reg=$reg_size",  true,  301 );  exit;
 
-echo "<script type='text/javascript'>window.top.location='../sel_tourney.php?team_id='$team_id'&team_reg='$reg_size';</script>"; exit;
+header("location:../sel_tourney.php?team_id=$team_id&team_reg=$reg_size",  true,  301 );  exit;
 
 ?>
