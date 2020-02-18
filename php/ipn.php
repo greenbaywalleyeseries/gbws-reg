f32e1bfa5ce470d0982e72838303f7dcd7a74893
Yami <gbwalleyeseries@gmail.com>
2020-02-17 22:59:00 -0600

    fixed IPN????

--- php/ipn.php
+++ php/ipn.php
@@ -136,22 +136,15 @@ $notify_version = $_POST['notify_version'];
 $transaction_subject = $_POST['transaction_subject'];
 $charset = $_POST['charset'];
 $discount = $_POST['discount'];
-mysqli_query($mysqli, "insert into log (message) values ('Before IPN test good')");
-if (isset($_POST['test_ipn'])) {
-    $test_ipn= $_POST['test_ipn'];
-} else {
-    $test_ipn='';
-}
-mysqli_query($mysqli, "insert into log (message) values ('After IPN test good')");
 
-$SQL="INSERT INTO transactions (txn_id, payer_id, payer_status, custom, payment_status, payer_email, first_name, last_name, address_name, address_street, address_city, address_state, address_zip, address_country, address_country_code, address_status, residence_country, payment_date, num_cart_items, mc_gross, mc_fee, mc_currency, ipn_track_id, verify_sign, txn_type, payment_type, receiver_email, receiver_id, notify_version, transaction_subject, charset, discount, test_ipn) 
-    VALUES('$txn_id', '$payer_id', '$payer_status', '$custom', '$payment_status', '$payer_email', '$first_name', '$last_name', '$address_name', '$address_street', '$address_city', '$address_state', '$address_zip', '$address_country', '$address_country_code', '$address_status', '$residence_country', '$payment_date', $num_cart_items, '$mc_gross', '$mc_fee', '$mc_currency', '$ipn_track_id', '$verify_sign', '$txn_type', '$payment_type', '$receiver_email', '$receiver_id', '$notify_version', '$transaction_subject', '$charset', '$discount', '$test_ipn')";
+$SQL="INSERT INTO transactions (txn_id, payer_id, payer_status, custom, payment_status, payer_email, first_name, last_name, address_name, address_street, address_city, address_state, address_zip, address_country, address_country_code, address_status, residence_country, payment_date, num_cart_items, mc_gross, mc_fee, mc_currency, ipn_track_id, verify_sign, txn_type, payment_type, receiver_email, receiver_id, notify_version, transaction_subject, charset, discount) 
+    VALUES('$txn_id', '$payer_id', '$payer_status', '$custom', '$payment_status', '$payer_email', '$first_name', '$last_name', '$address_name', '$address_street', '$address_city', '$address_state', '$address_zip', '$address_country', '$address_country_code', '$address_status', '$residence_country', '$payment_date', $num_cart_items, '$mc_gross', '$mc_fee', '$mc_currency', '$ipn_track_id', '$verify_sign', '$txn_type', '$payment_type', '$receiver_email', '$receiver_id', '$notify_version', '$transaction_subject', '$charset', '$discount')";
 
 mysqli_query($mysqli, "insert into log (message) values ('After SQL test good')");
 $file='sql.log';
 file_put_contents($file,$SQL);
 // Place the transaction header into the database
-mysqli_query($mysqli, $SQL) or die ("unable to execute the query1");
+mysqli_query($mysqli, $SQL) or die ("unable to execute the query");
 
 for ($i=1; $i<=$num_cart_items; $i++) {
     for ($i=1; $i<=$num_cart_items; $i++) {
