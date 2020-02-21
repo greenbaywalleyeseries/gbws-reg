e59c1758bcc7b7a9183c097bfe3e99f20f67c836
Yami <gbwalleyeseries@gmail.com>
2020-02-20 19:55:16 -0600

    fix javascript links on mailin_sel_tourney

--- admin/mailin/mailin_sel_tourney.php
+++ admin/mailin/mailin_sel_tourney.php
@@ -52,8 +52,7 @@
 <html lang="en">
 
   <head>
-  	<base href="http://127.0.0.1/gbws-reg/">
-  	<script src="js/jquery-3.3.1.min.js"></script>
+  	<script src="../../js/jquery-3.3.1.min.js"></script>
     <script type="text/javascript">
     	team_info= new Array();
     	var items = new Array();
@@ -90,17 +89,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link href="https://fonts.googleapis.com/css?family=Muli:400,700" rel="stylesheet">
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="../../fonts/icomoon/style.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="../../css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="../../css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../../css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../../fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="../../css/aos.css">
 
     <!-- MAIN CSS -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../../css/style.css">
 
   </head>
 
@@ -147,7 +146,7 @@
       </header>
 
 	<div class="ftco-blocks-cover-1">
-      <div class="site-section-cover overlay" style= "background-image: url('images/gbws_background.jpg')"> 
+      <div class="site-section-cover overlay" style= "background-image: url('../../images/gbws_background.jpg')"> 
         <div class="container">
         	<div class="row align-items-center justify-content-center">
             	<div class="col-md-12 text-center">
@@ -406,22 +405,22 @@
 
 
 
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/jquery-migrate-3.0.0.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.sticky.js"></script>
-    <script src="js/jquery.waypoints.min.js"></script>
-    <script src="js/jquery.animateNumber.min.js"></script>
-    <script src="js/jquery.fancybox.min.js"></script>
-    <script src="js/jquery.stellar.min.js"></script>
-    <script src="js/jquery.easing.1.3.js"></script>
-    <script src="js/bootstrap-datepicker.min.js"></script>
-    <script src="js/isotope.pkgd.min.js"></script>
-    <script src="js/aos.js"></script>
+    <script src="../../js/jquery-3.3.1.min.js"></script>
+    <script src="../../js/jquery-migrate-3.0.0.js"></script>
+    <script src="../../js/popper.min.js"></script>
+    <script src="../../js/bootstrap.min.js"></script>
+    <script src="../../js/owl.carousel.min.js"></script>
+    <script src="../../js/jquery.sticky.js"></script>
+    <script src="../../js/jquery.waypoints.min.js"></script>
+    <script src="../../js/jquery.animateNumber.min.js"></script>
+    <script src="../../js/jquery.fancybox.min.js"></script>
+    <script src="../../js/jquery.stellar.min.js"></script>
+    <script src="../../js/jquery.easing.1.3.js"></script>
+    <script src="../../js/bootstrap-datepicker.min.js"></script>
+    <script src="../../js/isotope.pkgd.min.js"></script>
+    <script src="../../js/aos.js"></script>
 
-    <script src="js/main.js"></script>
+    <script src="../../js/main.js"></script>
 
 
   </body>
