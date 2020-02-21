ef0749894c16ad385962eff9ed09c5b28f8be2fd
Yami <gbwalleyeseries@gmail.com>
2020-02-20 19:50:51 -0600

    fix links on list_team.php

--- admin/mailin/list_team.php
+++ admin/mailin/list_team.php
@@ -5,14 +5,14 @@
     	<meta charset="UTF-8">
     	<meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://fonts.googleapis.com/css?family=Muli:400,700" rel="stylesheet">
-        <link rel="stylesheet" href="fonts/icomoon/style.css">
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-        <link rel="stylesheet" href="css/jquery.fancybox.min.css">
-        <link rel="stylesheet" href="css/owl.carousel.min.css">
-        <link rel="stylesheet" href="css/owl.theme.default.min.css">
-        <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-        <link rel="stylesheet" href="css/aos.css">
+        <link rel="stylesheet" href="../../fonts/icomoon/style.css">
+        <link rel="stylesheet" href="../../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../../css/bootstrap-datepicker.css">
+        <link rel="stylesheet" href="../../css/jquery.fancybox.min.css">
+        <link rel="stylesheet" href="../../css/owl.carousel.min.css">
+        <link rel="stylesheet" href="../../css/owl.theme.default.min.css">
+        <link rel="stylesheet" href="../../fonts/flaticon/font/flaticon.css">
+        <link rel="stylesheet" href="../../css/aos.css">
     
         <!-- MAIN CSS -->
         <link rel="stylesheet" href="css/style.css">
