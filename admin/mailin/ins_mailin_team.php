60f2d5be41c6a538b6b0bdf1d3c87b0936efb8a7
Yami <gbwalleyeseries@gmail.com>
2020-03-28 22:02:15 -0500

    fix mailin registration

--- admin/mailin/ins_mailin_team.php
+++ admin/mailin/ins_mailin_team.php
@@ -139,10 +139,10 @@ echo 'alert("Problem registering team")';
 }
 
 $mysqli->close();\
-header("Location:../sel_tourney.php?team_id=$team_id&team_reg=$reg_size");
+header("Location:mailin_sel_tourney.php?team_id=$team_id&team_reg=$reg_size");
 
 echo "<br>";
 
-echo "<a href=../sel_tourney.php?team_id=".$team_id."&team_reg=".$reg_size.">Click here if not automatically redirected to tournament registration page</a>";
+echo "<a href=mailin_sel_tourney.php?team_id=".$team_id."&team_reg=".$reg_size.">Click here if not automatically redirected to tournament registration page</a>";
 
 ?>
