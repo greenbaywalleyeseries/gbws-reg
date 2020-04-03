d7446b6bb1e3ba87bac9b570a77680baac0533dd
Yami <gbwalleyeseries@gmail.com>
2020-04-03 17:10:18 -0500

    test

--- GB-Tourney/update_teams.php
+++ GB-Tourney/update_teams.php
@@ -7,6 +7,7 @@ $tourney_id='GB';
 $del_teams_SQL="delete from tourney_teams";
 
 $status=mysqli_query($mysqli_tourney,$del_teams_SQL);
+echo $status;
 if($status == false)
 {
     die("Teams not updated - unable to delete");
