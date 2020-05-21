2d363301801f491f2f86d724e6e05a93c8c6a446
Yami <gbwalleyeseries@gmail.com>
2020-05-20 23:57:48 -0500

    test

--- admin/get_champ_teams.php
+++ admin/get_champ_teams.php
@@ -14,7 +14,7 @@ include('..\php\gbws_reg_db.php');
 
 
 $sql="SELECT a.team_id, c.first as partner1_first, c.last as partner1_last, d.first as partner2_first, d.last as partner2_last, a.GB, a.DY, a.SB, (ifnull(a.GB,0) + ifnull(a.DY,0) + ifnull(a.SB,0) + ifnull(a.MAR,0)) as total_points FROM points as a join team_info as b on a.team_id=b.team_id join member_info as c on b.partner1=c.mbr_id join member_info as d on b.partner2=d.mbr_id having (total_points > 799 or (a.GB is not null and a.DY is not null and a.SB is not null)) order by total_points desc";
-
+$sql="SELECT * FROM points as a join team_info as b on a.team_id=b.team_id join member_info as c on b.partner1=c.mbr_id join member_info as d on b.partner2=d.mbr_id having (total_points > 799 or (a.GB is not null and a.DY is not null and a.SB is not null)) order by total_points desc";
 echo $sql;
 
 
@@ -29,7 +29,6 @@ echo '<table id="team">
 <th>Total Points</th>
 </tr>';
 while($row = mysqli_fetch_array($result)) {
-    print_r($row);
     $Team_ID=$row['team_id'];
     $Team_URL='<a href="tourney_reg.php?team_id=' .$Team_ID. '&new_team=">'.$Team_ID.'</a>';
     $Partner1=$row['partner1_first'] ." ".$row['partner1_last'];
