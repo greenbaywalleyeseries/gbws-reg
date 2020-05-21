3b9805ffbdefb55df3475f5aeda90d78b6ced27c
Yami <gbwalleyeseries@gmail.com>
2020-05-20 23:02:21 -0500

    remove schema from champ team list

--- admin/get_champ_teams.php
+++ admin/get_champ_teams.php
@@ -15,7 +15,7 @@ include('..\gbws_reg_db.php');
 
 mysqli_select_db($mysqli,"gbws_reg");
 $sql="SELECT a.team_id, c.first as partner1_first, c.last as partner1_last, d.first as partner2_first, d.last as partner2_last, a.GB, a.DY, a.SB, (ifnull(a.GB,0) + ifnull(a.DY,0) + ifnull(a.SB,0) + ifnull(a.MAR,0)) as total_points
-FROM gbws_reg.points as a
+FROM points as a
 join team_info as b
 	on a.team_id=b.team_id
 join member_info as c
