2e3fcf08157d9797c422659ce957fd1e63e5f0ec
Yami <gbwalleyeseries@gmail.com>
2020-05-20 21:31:04 -0500

    timezone set to central

--- test.php
+++ test.php
@@ -1,4 +1,9 @@
 <?php 
 echo date("m/d/Y h:i:s a", time());
 echo date_default_timezone_get();
+
+$today = new DateTime("now", new DateTimeZone('America/Chicago') );
+echo $today->format('Y-m-d h:i:s a');
+
+
 ?>
