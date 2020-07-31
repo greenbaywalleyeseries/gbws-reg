09d48910d1db2643e8392b40f5d86506a53daf32
Yami <gbwalleyeseries@gmail.com>
2020-07-30 22:26:18 -0500

    add sub table

--- admin/points_standings.php
+++ admin/points_standings.php
@@ -73,6 +73,63 @@ class PDF extends FPDF
                 $x++;
             }
     }
+    function Sub_Header()
+    {
+        // Logo
+        $this->Image('../images/gbws_logo-no-background.png',20,6,40,40);
+        // Arial bold 15
+        $this->SetFont('Arial','B',15);
+        // Move to the right
+        $this->Cell(50);
+        // Title
+        $this->Cell(100,8,'Green Bay Walleye Series',0,0,'C');
+        //2nd Image
+        $this->Image('../images/gbws_logo-no-background.png', 160,6,40,40);
+        // Line break
+        $this->Ln(15);
+        //Next Line for Tourney Location
+        // Arial bold 12
+        $this->SetFont('Arial','B',12);
+        $this->Cell(50);
+        $this->Cell(100,5,'2020 Points Standings',0,1,'C');
+        // Line break
+        //$this->Ln(20);
+        //Next Line for Tourney Dates
+        //$this->Cell(50);
+        //$this->Cell(100,5,$dates,0,0,'C');
+        // Line break
+        $this->Ln(15);
+        $tbl_header=array('Place','Team','Metro','Oconto','Sturgeon Bay','Marinette','Total Points');
+        $this->Setx(30);
+        $this->SetFont('Arial','B',8);
+        $this->Cell(12,5,$tbl_header[0],1,0,'C');
+        $this->Cell(55,5,$tbl_header[1],1,0,'C');
+        $this->Cell(20,5,$tbl_header[2],1,0,'C');
+        $this->Cell(20,5,$tbl_header[3],1,0,'C');
+        $this->Cell(20,5,$tbl_header[4],1,0,'C');
+        $this->Cell(20,5,$tbl_header[5],1,0,'C');
+        $this->Cell(20,5,$tbl_header[6],1,0,'C');
+        $this->Ln(5);
+    }
+}
+function SubTable($rankings)
+{
+    $this->Setx(50);
+    // Data
+    $x = 0;
+    //foreach($rankings as $row)
+    while ($x < count($rankings,0)) {
+        $this->Setx(30);
+        $this->Cell(12,5,$rankings[$x][1],1,0,'C');
+        $this->Cell(55,5,$rankings[$x][2],1,0,'C');
+        $this->Cell(20,5,$rankings[$x][3],1,0,'C');
+        $this->Cell(20,5,$rankings[$x][4],1,0,'C');
+        $this->Cell(20,5,$rankings[$x][5],1,0,'C');
+        $this->Cell(20,5,$rankings[$x][6],1,0,'C');
+        $this->Cell(20,5,$rankings[$x][7],1,0,'C');
+        $this->Ln();
+        $x++;
+    }
 }
 
 $rankings=array();
@@ -130,6 +187,42 @@ order by total_points desc";
     } else {
         echo "0 results";
     }
+    
+    $sub_rankings=array();
+    
+    $sub_sql="SELECT a.team_id,
+	concat(c.first, ' ', c.last) as Partner1,
+	concat(d.first, ' ', d.last) as Partner2,
+	a.GB,
+    a.DY,
+    a.SB,
+    a.MAR,
+    (ifnull(a.GB,0) + ifnull(a.DY,0) + ifnull(a.SB,0) + ifnull(a.MAR,0)) as total_points
+FROM gbws_reg.points as a
+	join gbws_reg.team_info as b on a.team_id=b.team_id
+    inner join gbws_reg.member_info c on ( b.partner1=c.mbr_id)
+    inner join gbws_reg.member_info d on (b.partner2=d.mbr_id)
+    left join gbws_reg.temp_subs e on a.team_id=e.team_id
+where e.team_id IS NULL
+order by total_points desc";
+    
+    $sub_result = $mysqli->query($sub_sql);
+    if ($sub_result->num_rows > 0) {
+        while($row = $sub_result->fetch_assoc()) {
+            $Partners=$row["Partner1"] . " & " .$row["Partner2"];
+            $GB=$row["GB"];
+            $DY=$row["DY"];
+            $SB=$row["SB"];
+            $MAR=$row["MAR"];
+            $total_points=$row["total_points"];
+            $data=array('*','*', $Partners, $GB, $DY, $SB, $MAR, $total_points);
+            $sub_rankings[]=$data;
+        }
+    } else {
+        echo "0 results";
+    }
+    
+    
 
 $mysqli->close();
 
@@ -143,6 +236,9 @@ $pdf->SetFont('Arial','',8);
 $pdf->AddPage();
 
 $pdf->BasicTable($rankings);
+$pdf->AddPage();
+
+$pdf->SubTable($sub_rankings);
 $pdf->AliasNbPages();
 $pdf->Output();
 
