5df6b8aa35c2a165d3347fe47ee70fc600c0972a
Yami <gbwalleyeseries@gmail.com>
2022-03-15 10:40:23 -0500

    fix date_time comparison test

--- test/date_time.php
+++ test/date_time.php
@@ -4,18 +4,26 @@
 //echo $date;
 
 
-$curr_time = date('d-m-y h:i:s');
+//$curr_time = date('d-m-y h:i:s');
+$curr_time = date('Y-m-d H:m');
 $b = '2013-08-10 12:00';
 
 $dateB = DateTime::createFromFormat('Y-m-d H:m', $b);
 
+echo strtotime($curr_time);
+echo "<br>";
+echo strtotime($b);
+echo "<br>";
 
-if ($curr_time < strtotime($b)) {
+if (strtotime($curr_time) > strtotime($b)) {
     echo $curr_time;
 }
 
 else {
     echo $b;
 }
+
+echo "<br";
+echo strtotime($curr_time);
 
 ?>
