91d6366d2d30679c8b203eaef2e7b4da7c60c194
Yami <gbwalleyeseries@gmail.com>
2023-01-19 14:39:32 -0600

    add some original code

--- admin/generate_tourney_roster_pdf.php
+++ admin/generate_tourney_roster_pdf.php
@@ -4,19 +4,121 @@ require('../pdf/fpdf.php');
 $tourney_id = $_GET['tourney_id'];
 
 
-echo "This is a test";
+class PDF extends FPDF
+{
+    // Page header
+    function Header()
+    {
+        include('../php/gbws_reg_db.php');
+        $tourney_id = $_GET['tourney_id'];
+        
+        $location_sql = "select location, DATE_FORMAT(start_date, '%M %d, %Y') as start_date,  DATE_FORMAT(second_date, '%M %d, %Y')as second_date from tourneyinfo where local='".$tourney_id."'";
+        $location_result = $mysqli->query($location_sql);
+        foreach ($location_result as $row):
+        $location=$row['location'];
+        $start_date=$row['start_date'];
+        $second_date=$row['second_date'];
+        
+        
+        endforeach;
+        
+        if (isset($second_date)) {
+            $date=$start_date ." - " . $second_date;
+        } else {
+            $date=$start_date;
+        }
+        
+        // Logo
+        $this->Image('../images/gbws_logo-no-background.png',35,6,40,40);
+        // Arial bold 15
+        $this->SetFont('Arial','B',15);
+        // Move to the right
+        $this->Cell(50);
+        // Title
+        $this->Cell(100,8,'Green Bay Walleye Series',0,0,'C');
+        //2nd Image
+        $this->Image('../images/gbws_logo-no-background.png', 140,6,40,40);
+        // Line break
+        $this->Ln(15);
+        //Next Line for Tourney Location
+        // Arial bold 12
+        $this->SetFont('Arial','B',12);
+        $this->Cell(50);
+        $this->Cell(100,5,$location,0,1,'C');
+        // Line break
+        //$this->Ln(20);
+        //Next Line for Tourney Dates
+        $this->Cell(50);
+        $this->Cell(100,5,$date,0,0,'C');
+        // Header
+        $this->Setx(42);
+        
+        $this->Ln();
+        // Line break
+        $this->Ln(15);
+        $this->Setx(42);
+        $this->SetFont('Arial','B',8);
+        $tbl_header=array('Boat #','Partner 1','Partner 2','Option Pot','Big Fish');
+        $this->Cell(15,5,$tbl_header[0],1,0,'C');
+        $this->Cell(40,5,$tbl_header[1],1,0,'C');
+        $this->Cell(40,5,$tbl_header[2],1,0,'C');
+        $this->Cell(15,5,$tbl_header[3],1,0,'C');
+        $this->Cell(20,5,$tbl_header[4],1,0,'C');
+        $this->Ln(5);
+    }
+    
+    // Page footer
+    function Footer()
+    {
+        // Position at 1.5 cm from bottom
+        $this->SetY(-15);
+        // Arial italic 8
+        $this->SetFont('Arial','I',8);
+        // Page number
+        $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+    }
+    function BasicTable($roster)
+    {
+        
+        // Data
+        $x = 0;
+        //foreach($rankings as $row)
+        while ($x < count($roster,0)) {
+            $this->Setx(42);
+            $this->Cell(15,5,$roster[$x][0],1,0,'C');
+            $this->Cell(40,5,$roster[$x][1],1,0,'C');
+            $this->Cell(40,5,$roster[$x][2],1,0,'C');
+            $this->Cell(15,5,$roster[$x][3],1,0,'C');
+            $this->Cell(20,5,$roster[$x][4],1,0,'C');
+            $this->Ln();
+            $x++;
+        }
+    }
+}
 
-// Instanciation of inherited class
-//$pdf = new PDF('P','mm','Legal');
-//$pdf = new PDF('P','mm','Letter');
-//$pdf->SetLeftMargin(3);
-// Column headings
-//$pdf->SetFont('Arial','',8);
-//$pdf->AddPage();
+$roster=array();
 
-//$pdf->BasicTable($roster);
-//$pdf->AliasNbPages();
-//$pdf->Output();
+$sql="call ListTourneyRoster('".$tourney_id."')";
+$result = mysqli_query($mysqli,$sql);
+$i=1;
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        
+        $boat_num=$i;
+        $partner1=$row["partner1"];
+        $partner2=$row["partner2"];
+        $option_pot=$row["option_pot"];
+        $big_fish=$row["big_fish"];
+        
+        $data=array($boat_num, $partner1, $partner2, $option_pot, $big_fish);
+        $roster[]=$data;
+        $i=$i+1;
+        
+    }
+    
+}
 
+
+$mysqli->close();
 
 ?>
