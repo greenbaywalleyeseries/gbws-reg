b69ea6b56386ceb138eb0a911a35fcea4db4550c
Yami <gbwalleyeseries@gmail.com>
2023-01-19 13:04:02 -0600

    tourney roster fixes - cont...

--- admin/generate_tourney_roster_pdf.php
+++ admin/generate_tourney_roster_pdf.php
@@ -17,7 +17,20 @@ include('../php/gbws_reg_db.php');
 require('../pdf/fpdf.php');
 $tourney_id = $_GET['tourney_id'];
 
-echo $tourney_id
+$location_sql = "select location, DATE_FORMAT(start_date, '%M %d, %Y') as start_date,  DATE_FORMAT(second_date, '%M %d, %Y')as second_date from tourneyinfo where local='".$tourney_id."'";
+$location_result = $mysqli->query($location_sql);
+foreach ($location_result as $row):
+$location=$row['location'];
+$start_date=$row['start_date'];
+$second_date=$row['second_date'];
+
+
+endforeach;
+
+if (isset($second_date)) {
+    $date=$start_date ." - " . $second_date;
+} else {
+    $date=$start_date;
 
 
 ?>
