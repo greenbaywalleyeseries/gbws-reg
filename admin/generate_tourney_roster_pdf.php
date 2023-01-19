f8d326b2089e63662b457810d13537e448423b02
Yami <gbwalleyeseries@gmail.com>
2023-01-19 14:31:34 -0600

    test 2

--- admin/generate_tourney_roster_pdf.php
+++ admin/generate_tourney_roster_pdf.php
@@ -4,19 +4,19 @@ require('../pdf/fpdf.php');
 $tourney_id = $_GET['tourney_id'];
 
 
-
+echo "This is a test";
 
 // Instanciation of inherited class
-$pdf = new PDF('P','mm','Legal');
+//$pdf = new PDF('P','mm','Legal');
 //$pdf = new PDF('P','mm','Letter');
-$pdf->SetLeftMargin(3);
+//$pdf->SetLeftMargin(3);
 // Column headings
-$pdf->SetFont('Arial','',8);
-$pdf->AddPage();
+//$pdf->SetFont('Arial','',8);
+//$pdf->AddPage();
 
-$pdf->BasicTable($roster);
-$pdf->AliasNbPages();
-$pdf->Output();
+//$pdf->BasicTable($roster);
+//$pdf->AliasNbPages();
+//$pdf->Output();
 
 
 ?>
