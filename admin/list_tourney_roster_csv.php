8ba3040a1ca37c70415c9ece50abb8cdcceaedae
mguch <mguch@Walleye.home.arpa>
2023-02-22 20:25:51 -0600

    REST API to provide CSV of tourney entries

---
+++ admin/list_tourney_roster_csv.php
@@ -0,0 +1,24 @@
+<?php
+    include('../php/gbws_reg_db.php');
+    print "{\"data\": ";
+    //$tourney_id = 'T1';
+    $tourney_id = $_GET['tourney_id'];
+    
+    mysqli_select_db($mysqli,"gbws_reg");
+    
+    $sql="call ListTourneyRoster('".$tourney_id."')";
+    
+    $results = mysqli_query($mysqli,$sql);
+    
+    $rows = array();
+    if(mysqli_num_rows($results) > 0){
+        while($record = mysqli_fetch_assoc($results)) {
+            array_push($rows, $record);
+        }
+        print json_encode($rows);
+    }
+    else {
+        echo("No records found");
+    }
+    print "}";
+?>
