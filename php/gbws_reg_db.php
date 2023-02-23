e09776a161df07c907e232f898dcda3a54ffce46
Guch <gbwalleyeseries@gmail.com>
2023-02-22 20:46:59 -0600

    Fix connections

--- php/gbws_reg_db.php
+++ php/gbws_reg_db.php
@@ -1,13 +1,13 @@
 <?php
-$host = "gbws-reg-db.mysql.database.azure.com";
-$username = "yami@gbws-reg-db";
-$password = "Th1sSucksa$$";
-$db_name = "gbws_reg";
-
-//$host = "localhost";
-//$username = "root";
+//$host = "";
+//$username = "";
 //$password = "";
-//$db_name = "gbws_reg";
+//$db_name = "";
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$db_name = "gbws_reg";
 
 // Create connection for mysqli extension
 $mysqli = new mysqli($host, $username, $password, $db_name);
